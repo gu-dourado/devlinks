5db12be14e30ab1cd577a1cc6b1f58c4709cded1
Gustavo Dourado <rm551921@fiap.com.br>
2023-07-25 22:34:18 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de perfil de Gustavo Dourado utilizando uma jaqueta e camisa social no fundo claro."
+    )
+    // se tiver sem light mode, manter a imagem normal
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de perfil de Gustavo Dourado utilizando uma jaqueta e camisa social no fundo escuro."
+    )
+  }
+}
+// if else toggle
+//if (html.classList.contains("light")) {
+//    html.classList.remove("light")
+//  } else {
+//  html.classList.add("light")
+//}
