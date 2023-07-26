9e6624e7fab8648bda55ea03a38dd4ebd8163f99
Gustavo Dourado <rm551921@fiap.com.br>
2023-07-26 01:40:45 -0300

    :tada: initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("light")
+  html.classList.toggle("lighta")
   // pegar a tag img
   const img = document.querySelector("#profile img")
   // substituir a imagem
